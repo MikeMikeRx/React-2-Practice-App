e86d911d68720a7c6a51eceffaf017033fa2963a
MichalRais <Michal.Rais@iCloud.com>
2025-07-03 14:18:50 +0100

    Add Submit function

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
-import React from 'react'
+import { useState } from "react"
 
 const App = () => {
+  const [fullName, setFullName] = useState("")
+  const [email, setEmail] = useState("")
+  const [users, setUsers] = useState([])
+
+  const formSubmit = (e)=>{
+    e.preventDefault()
+    console.log("Form sent")
+  }
+
+
   return <article>
-    <form>
+    <form onSubmit={formSubmit}>
       <input className="userInfo" type="text" placeholder="Name" />
       <input className="userInfo" type="email" placeholder="Email"/>
       <input type="submit"/>
