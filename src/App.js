821c233124d65d645847bfb08e824f5cee867442
MichalRais <Michal.Rais@iCloud.com>
2025-07-03 15:05:06 +0100

    Add input Age

--- src/App.js
+++ src/App.js
@@ -3,13 +3,14 @@ import { useState } from "react"
 const App = () => {
   const [fullName, setFullName] = useState("")
   const [email, setEmail] = useState("")
+  const [age, setAge] = useState ("")
   const [users, setUsers] = useState([])
 
   const formSubmit = (e)=>{
     e.preventDefault()
 
-    if (fullName && email) {
-      const oneUser = {fullName, email}    //{fullName: fullName, email: email }
+    if (fullName && email && age) {
+      const oneUser = {fullName, email, age}    //{fullName: fullName, email: email, age: age}
       setUsers( (users)=> {
         return [...users, oneUser]
       })
@@ -19,6 +20,7 @@ const App = () => {
 
     setFullName("")
     setEmail("")
+    setAge ("")
 
   }
 
@@ -40,15 +42,24 @@ const App = () => {
       onChange={ (e)=> setEmail(e.target.value) }
       />
 
+      <input 
+      className="userInfo" 
+      type="number" 
+      placeholder="Age"
+      value={age}
+      onChange={ (e)=> setAge(e.target.value) }
+      />
+
       <input type="submit"/>
     </form>
 
     {users.map( (oneUser, index) => {
-      const {fullName, email} = oneUser
+      const {fullName, email, age} = oneUser
 
       return <div className="item" key={index}>
         <h4>{fullName}</h4>
         <p>{email}</p>
+        <p>{age}</p>
       </div>
     })}
 
