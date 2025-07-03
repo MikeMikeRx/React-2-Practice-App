4bd141287b7de6b466247c07eb6a02b8c6178b39
MichalRais <Michal.Rais@iCloud.com>
2025-07-03 13:11:13 +0100

    Rest Operator

--- src/App.js
+++ src/App.js
@@ -1,34 +1,18 @@
-//Spread operator
-
-//Consolidtaion
-const studentGroup = ["Michal", "Martin", "Daniel", "Alyce"]
-const allStudents = ["Paul", "Claudia", ...studentGroup, "Francis"]
-
-//Function arguments
-const numbers = [1, 2, 3]
-const multipleNumbers = (a, b, c)=> {
-  console.log(a * b * c)
+//Rest Operator
+const sum = (...allNumbers) => {
+  let result = 0
+  for (const oneNumber of allNumbers){
+    // result = result + oneNumber
+    result += oneNumber
+  }
+  return result
 }
-multipleNumbers(...numbers)
 
-//Array copy
-const students1 = ["Martin", "Daniel", "Alyce"]
-const students2 = [...students1]
-students2.push("Paul")
-// console.log(students1) //(3) ['Martin', 'Daniel', 'Alyce']
-// console.log(students2) //(4) ['Martin', 'Daniel', 'Alyce', 'Paul']
-
-//Concatenation (linking or joining things together in a sequence)
-const names1 = ["Daniel", "Alyce"]
-const names2 = ["Paul", "Claudia"]
-const newArray1 = names1.concat(names2)
-
-newArray1.push("Paul")
-console.log(newArray1) //(5) ['Daniel', 'Alyce', 'Paul', 'Claudia', 'Paul']
-
-const newArray2 = [...names1, "Alexandara", ...names2]
-console.log(newArray2) //(5) ['Daniel', 'Alyce', 'Alexandara', 'Paul', 'Claudia']
-
+console.log(sum(1, 2))
+console.log(sum(1, 2, 3))
+console.log(sum(1, 2, 5, 10))
+console.log(sum(1, 2, 10, 10, 10, 159, 234, 1112 ))
+//I can add how many numbers I need ...
 
 //-----------------------------------------------------------------------
 const App = () => {
