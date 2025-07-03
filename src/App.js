a791f29f3cd7280792bbf8a49655585cd107bf74
MichalRais <Michal.Rais@iCloud.com>
2025-07-03 11:08:55 +0100

    Added inputs

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 const App = () => {
- return (
-  
- )
+ return <article>
+    <form>
+      <div>
+        <input type="text" id="userName"placeholder="Name"/>
+        <input type="Submit" />
+      </div>
+    </form>
+ </article>  
 }
 
-export default App;
+export default App
