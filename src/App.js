932e7a6daacff959db957ddc0988f2e957918cc5
MichalRais <Michal.Rais@iCloud.com>
2025-07-03 16:05:11 +0100

    Dynamic Object - theory

--- src/App.js
+++ src/App.js
@@ -1,3 +1,21 @@
+//Dynamic Object
+
+// const info = "favoriteColor" // Add favoriteColor: red
+const info = "email" // Update email to ..enmail: red
+
+const person = {
+  fullName: "Michal Coder",
+  email: "michal@codex.com"
+}
+
+// person.age = 30
+// console.log(person) // {fullName: 'Michal Coder', email: 'michal@codex.com', age: 30}
+
+person[info] = "red"
+console.log(person) // {fullName: 'Michal Coder', email: 'michal@codex.com', favoriteColor: 'red'}
+
+
+
 
 const App = () => {
 return <div>
