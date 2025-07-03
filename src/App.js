e88020ad7fe32347baa690b22eba1c6600648f8a
MichalRais <Michal.Rais@iCloud.com>
2025-07-03 13:20:03 +0100

    Start - form

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
+import React from 'react'
+
 const App = () => {
- return (
+  return <article>
+    <form>
+      <input className="userInfo" type="text" placeholder="Name" />
+      <input className="userInfo" type="email" placeholder="Email"/>
+      <input type="submit"/>
+    </form>
+  </article>
   
- )
 }
 
-export default App;
+export default App
